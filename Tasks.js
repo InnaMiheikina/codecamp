aadb4f51e29f5ed0cf13dd14132d5051e4968c44
InnaMih212 <miheikinainna212@gmail.com>
2022-01-21 23:05:58 +0300

    11

--- Tasks.js
+++ Tasks.js
@@ -1,2 +1,4 @@
-let catName = "Oliver";
-let catSound = "Meow!";
+const FCC = "freeCodeCamp"; // Змініть цей рядок
+let fact = "is cool!"; // Змініть цей рядок
+fact = "is awesome!";
+console.log(FCC, fact); // Змініть цей рядок
