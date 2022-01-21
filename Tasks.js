bf5efcbd21f6bcf5617efdfbfef0de6e7fef1b1d
InnaMih212 <miheikinainna212@gmail.com>
2022-01-21 22:59:14 +0300

    10

--- Tasks.js
+++ Tasks.js
@@ -1,9 +1,2 @@
-// Оголошення змінної
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-// Призначення змінної
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+let catName = "Oliver";
+let catSound = "Meow!";
