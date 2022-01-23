d200e251f0d09b1ba3ca5c45cbb3941d3e07a950
InnaMih212 <miheikinainna212@gmail.com>
2022-01-23 23:11:51 +0300

    37

--- Tasks.js
+++ Tasks.js
@@ -1,8 +1,2 @@
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
-
 // Only change code below this line
-const wordBlanks = "Very " + "myAdjective " + "and evil " + "myNoun " + "on the way to " + "myVerb "  + "very " + "myAdverb" + "."; // Change this line
-// Only change code above this line
+const myArray = ["hello", 5];
