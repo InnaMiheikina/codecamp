14288f63a598bbb1dece14733d3f38e55a537b19
InnaMih212 <miheikinainna212@gmail.com>
2022-01-23 23:08:55 +0300

    36f

--- Tasks.js
+++ Tasks.js
@@ -1,5 +1,8 @@
-// Setup
-const lastName = "Lovelace";
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
 
 // Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+const wordBlanks = "Very " + "myAdjective " + "and evil " + "myNoun " + "on the way to " + "myVerb "  + "very " + "myAdverb" + "."; // Change this line
+// Only change code above this line
