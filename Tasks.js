a1d0534b684d784908a63c22e5f7f706bf8271c0
InnaMih212 <miheikinainna212@gmail.com>
2022-01-26 12:34:50 +0300

    78

--- Tasks.js
+++ Tasks.js
@@ -1,39 +1,8 @@
-var count = 0;
-
-function cc(card) {
+const myDog = {
     // Only change code below this line
-
-    var msg = '';
-
-    switch (card) {
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-            count++;
-            break;
-        case 7:
-        case 8:
-        case 9:
-            count = count;
-            break;
-        case 10:
-        case "J":
-        case "Q":
-        case "K":
-        case "A":
-            count--;
-            break;
-    }
-
-    if (count === 5) msg = "5 Bet";
-    else if (count === 0) msg = "0 Hold";
-    else if (count === -5) msg = "-5 Hold";
-    else if (count === -1) msg = "-1 Hold";
-    else if (count === 1) msg = "1 Bet";
-
-
-    return msg;
+    name: "Shtile",
+    legs: 4,
+    tails: 1,
+    friends: [1, "Nono"]
     // Only change code above this line
-}
+};
