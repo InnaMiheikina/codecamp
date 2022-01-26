66ad0668772b94dc2f87c6fff6fd0dbd20858851
InnaMih212 <miheikinainna212@gmail.com>
2022-01-26 10:56:32 +0300

    72

--- Tasks.js
+++ Tasks.js
@@ -1,22 +1,22 @@
-
-    function caseInSwitch(val) {
-        let answer = " " ;
-        // Only change code below this line
-        switch (val) {
-            case 1:
-                answer ="alpha";
-                break;
-            case 2:
-                answer ="beta";
-                break;
-            case 3:
-                answer = "gamma";
-                break;
-            case 4:
-                answer ="delta";
-                break;
-        }
-        // Only change code above this line
-        return answer;
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val) {
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+            break;
     }
-    caseInSwitch(1);
+    // Only change code above this line
+    return answer;
+}
+
+switchOfStuff(1);
