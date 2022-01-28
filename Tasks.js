202e63028e1155676977138b785fbf9d0a6fc7de
InnaMih212 <miheikinainna212@gmail.com>
2022-01-28 14:35:12 +0300

    95

--- Tasks.js
+++ Tasks.js
@@ -1,9 +1,7 @@
-// Setup
-const myArray = [];
-let i = 10;
-do {
-// Only change code below this line
-
-    myArray.push(i);
-    i++;
-} while (i < 5);
+function sum(arr, n) {
+    if (--n<0) {
+        return 0;
+    } else {
+        return sum(arr, n) + arr[n];
+    }
+}
