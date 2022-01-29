83657dfbbecf5fd3d3f3eb0ad0abe2de4a367e61
InnaMih212 <miheikinainna212@gmail.com>
2022-01-29 20:53:49 +0300

    111

--- Tasks.js
+++ Tasks.js
@@ -1,2 +1,15 @@
-let wordBlanks = "Once there was a " + myNoun + " which was very " + myAdjective + ". ";
-wordBlanks += "It " + myVerb + " " + myAdverb + " around the yard.";
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+
+    if (strokes == 1) return names[0];
+    else if (strokes <= par - 2) return names[1];
+    else if (strokes == par - 1) return names[2];
+    else if (strokes == par) return names[3];
+    else if (strokes == par + 1) return names[4];
+    else if (strokes == par + 2) return names[5];
+    else if (strokes >= par + 3) return names[6];
+
+    return "Change Me"; // in case: if non-valid input given
+}
+
+console.log(golfScore(5, 4));
