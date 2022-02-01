65ebb59fa97f47544cb0623525cfa41fc9767344
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-02-01 13:53:30 +0300

    13s

--- Tasks.js
+++ Tasks.js
@@ -1,6 +1,8 @@
-a *= 5;
-b *= 3;
-c *= 10;
-a /= 12;
-b /= 4;
-c /= 11;
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [,,...arr] = list; // Change this line
+    // Only change code above this line
+    return arr;
+}
+const arr = removeFirstTwo(source);
