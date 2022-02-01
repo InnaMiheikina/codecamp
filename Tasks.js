c88121a5e39d499f9da44325ea3aa3859581c7be
InnaMiheikina <InnaMiheikina93@gmail.com>
2022-02-01 15:17:03 +0300

    17s(2)

--- Tasks.js
+++ Tasks.js
@@ -1,9 +1,10 @@
+// Only change code below this line
 const bicycle = {
     gear: 2,
-    setGear(newGear){ //by removing the "function"
-        "use strict";
-        this.gear = newGear;
+    setGear(newGear) {
+        `${this.gear = newGear}`;
     }
 };
+// Only change code above this line
 bicycle.setGear(3);
 console.log(bicycle.gear);
